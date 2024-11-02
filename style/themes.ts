5555016789fef950630d1b69a4dce84a3f417ca9
Santi Tabbach <tabbach97@gmail.com>
2024-11-02 15:02:30 -0300

    Add theme

---
+++ style/themes.ts
@@ -0,0 +1,12 @@
+export const Theme = {
+	colors: {
+		cobalite: '#9F97F8',
+		sky: '#7BD7FE',
+		'glitter-shower': '#8FFAFF',
+		'light-tuquoise': '#87EFCC',
+		'herb-blend': '#4C8768',
+		'lava-stone': '#3C434F',
+		gray: '#E0E0E0',
+		red: '#f86868',
+	},
+};
